4e376f9b1b4ab5fa9f32181310ac796c884c89b1
dodocie <65538313@qq.com>
2017-10-29 10:17:58 +0800

    the project

--- config/index.js
+++ config/index.js
@@ -11,7 +11,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/dist/',
+    assetsPublicPath: './',
     productionSourceMap: false, //默认true，记得改为false。不然build的时候会生成超恶心的map文件
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
